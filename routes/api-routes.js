8829fcf7ab6d5452b51a3621f54b2d03a913875a
KarrynS <karrynsaw@gmail.com>
2021-03-25 22:38:45 +1100

    adding .aggregate & sum

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,16 +37,40 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 //get workout in range
-router.get("/api/workouts/range", (req,res) => {
-    Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-})
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    }
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 
 module.exports = router;
 
+
+
+/*
+//get workout in range
+router.get("/api/workouts/range", (req,res) => {
+  Workout.find({})
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+})
+*/
